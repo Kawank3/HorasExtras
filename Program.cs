ca0aae1ea18f350372386f93652095945c257bd6
Kawan Weber <kawan_weber@hotmail.com>
2025-05-18 12:47:57 -0300

    HORASEXTRAS

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.Write("Salário por hora: ");
+double salarioHora = double.Parse(Console.ReadLine());
+
+Console.Write("Horas trabalhadas: ");
+double horasTrabalhadas = double.Parse(Console.ReadLine());
+
+Console.Write("Horas extras: ");
+double horasExtras = double.Parse(Console.ReadLine())!;
+
+
+double salario = CalcularSalario(salarioHora, horasTrabalhadas, horasExtras);
+
+
+Console.WriteLine("Salário total: R$ " + salario.ToString("F2"));
+
+static double CalcularSalario(double salarioHora, double horasTrabalhadas, double horasExtras)
+{
+    double pagamentoNormal = salarioHora * horasTrabalhadas;
+    double pagamentoExtras = horasExtras * salarioHora * 1.4;
+    return pagamentoNormal + pagamentoExtras;
+}
+
